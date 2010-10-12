fd7337ef829fd73a9115f8f4f702069fbbb20c15
marcus256 <marcus256@919d6115-40b2-4a68-9893-b32b752ddb1a>
2010-10-12 13:20:28 +0000

    Started working on the encoder.
    git-svn-id: svn://svn.code.sf.net/p/liblzg/code/trunk@8 919d6115-40b2-4a68-9893-b32b752ddb1a

--- src/lib/encode.c
+++ src/lib/encode.c
@@ -25,22 +25,86 @@
 *    distribution.
 */
 
+#include <stdlib.h>
+#include <string.h>
 #include "internal.h"
 
 
 /*-- PRIVATE -----------------------------------------------------------------*/
 
-static int _LZG_SetHeader(const unsigned char *out, unsigned int outsize,
-  lzg_header *hdr)
+static void _LZG_SetHeader(unsigned char *out, lzg_header *hdr)
 {
-    /* Too small buffer? */
-    if (outsize < LZG_HEADER_SIZE)
+    /* Magic number */
+    out[0] = 'L';
+    out[1] = 'Z';
+    out[2] = 'G';
+
+    /* Input buffer size */
+    out[3] = hdr->encodedSize >> 24;
+    out[4] = hdr->encodedSize >> 16;
+    out[5] = hdr->encodedSize >> 8;
+    out[6] = hdr->encodedSize;
+
+    /* Output buffer size */
+    out[7] = hdr->decodedSize >> 24;
+    out[8] = hdr->decodedSize >> 16;
+    out[9] = hdr->decodedSize >> 8;
+    out[10] = hdr->decodedSize;
+
+    /* Checksum */
+    hdr->checksum = _LZG_CalcChecksum(&out[LZG_HEADER_SIZE], hdr->encodedSize);
+    out[11] = hdr->checksum >> 24;
+    out[12] = hdr->checksum >> 16;
+    out[13] = hdr->checksum >> 8;
+    out[14] = hdr->checksum;
+
+    /* Method */
+    out[15] = hdr->method;
+}
+
+typedef struct _hist_rec {
+    unsigned int count;
+    unsigned int symbol;
+} hist_rec;
+
+static int hist_rec_compare(const void *p1, const void *p2)
+{
+    return ((hist_rec*)p1)->count - ((hist_rec*)p2)->count;
+}
+
+static int _LZG_DetermineMarkers(const unsigned char *in, unsigned int insize,
+    unsigned char *leastCommon1, unsigned char *leastCommon2,
+    unsigned char *leastCommon3)
+{
+    hist_rec *hist;
+    unsigned int i;
+    unsigned char *src;
+
+    /* Allocate memory for histogram */
+    hist = (hist_rec*) malloc(sizeof(hist_rec) * 256);
+    if (!hist)
         return FALSE;
 
-    /* FIXME */
-    (void) out;
-    (void) outsize;
-    (void) hdr;
+    /* Build histogram, O(n) */
+    for (i = 0; i < 256; ++i)
+    {
+        hist[i].count = 0;
+        hist[i].symbol = i;
+    }
+    src = (unsigned char *) in;
+    for (i = 0; i < insize; ++i)
+        hist[*src++].count++;
+
+    /* Sort histogram */
+    qsort((void *)hist, 256, sizeof(hist_rec), hist_rec_compare);
+
+    /* Find least common symbols */
+    *leastCommon1 = (unsigned char) hist[0].symbol;
+    *leastCommon2 = (unsigned char) hist[1].symbol;
+    *leastCommon3 = (unsigned char) hist[2].symbol;
+
+    /* Free memory for histogram */
+    free(hist);
 
     return TRUE;
 }
@@ -50,21 +114,55 @@ static int _LZG_SetHeader(const unsigned char *out, unsigned int outsize,
 
 unsigned int LZG_MaxEncodedSize(unsigned int insize)
 {
-    return insize + (insize / 64) + LZG_HEADER_SIZE + 3;
+    return insize + ((insize + 63) / 64) + LZG_HEADER_SIZE + 3;
 }
 
 unsigned int LZG_Encode(const unsigned char *in, unsigned int insize,
     unsigned char *out, unsigned int outsize)
 {
-    /* FIXME */
-    (void) in;
-    (void) insize;
-    (void) out;
-    (void) outsize;
+    unsigned char *src, *in_end, *dst, *out_end;
+    unsigned char copy3marker, copy4marker, copyNmarker;
     lzg_header hdr;
-    _LZG_SetHeader(out, outsize, &hdr);
+
+    /* Too small output buffer? */
+    if (outsize < LZG_HEADER_SIZE)
+        return 0;
+
+    /* Calculate histogram and find optimal marker symbols */
+    if (!_LZG_DetermineMarkers(in, insize, &copy3marker, &copy4marker, &copyNmarker))
+        return 0;
+
+    /* Initialize the byte streams */
+    src = (unsigned char *)in;
+    in_end = ((unsigned char *)in) + insize;
+    dst = out;
+    out_end = out + outsize;
+
+    /* Skip header information */
+    dst += LZG_HEADER_SIZE;
+
+    /* Set marker symbols */
+    if ((dst + 3) > out_end) return 0;
+    *dst++ = copy3marker;
+    *dst++ = copy4marker;
+    *dst++ = copyNmarker;
+
+    /* Main compression loop */
+    while (src < in_end)
+    {
+        if (dst >= out_end) return 0;
+
+        /* FIXME */
+        src++;
+    }
+
+    /* Set header data */
+    hdr.method = LZG_METHOD_LZG1;
+    hdr.encodedSize = (dst - out) - LZG_HEADER_SIZE;
+    hdr.decodedSize = insize;
+    _LZG_SetHeader(out, &hdr);
 
     /* Return size of compressed buffer */
-    return 0;
+    return LZG_HEADER_SIZE + hdr.encodedSize;
 }
 
